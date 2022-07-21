d81219a5f5dee41809c32ad83ec8dcd36fd21340
Tiity <anthony.cadet@ocito.com>
2022-07-21 16:10:50 +0200

    class palindrome

--- src/main/java/fr/m2i/javaunittest/JavaUnitTest.java
+++ src/main/java/fr/m2i/javaunittest/JavaUnitTest.java
@@ -3,6 +3,7 @@ package fr.m2i.javaunittest;
 public class JavaUnitTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("'kayak' est un palindrome ? " + Palindrome.isPalindrome("kayak"));
+        System.out.println("'bonjour' est un palindrome ? " + Palindrome.isPalindrome("bonjour"));
     }
 }
---
+++ src/main/java/fr/m2i/javaunittest/Palindrome.java
@@ -0,0 +1,27 @@
+package fr.m2i.javaunittest;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Palindrome {
+    
+    public static boolean isPalindrome(String phrase) {
+
+        phrase = phrase.toLowerCase();
+        phrase = phrase.replaceAll(" ", "");
+        phrase = StringUtils.stripAccents(phrase);
+        
+        int start = 0;
+        int end = phrase.length() - 1;
+
+        while (start < end) {
+            if (phrase.charAt(start) != phrase.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
+}
